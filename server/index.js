4c6831ec332e9bb0dacded11638501a730d40365
MrKazii <sandeshbasnet.077@kathford.edu.np>
2024-11-12 16:44:57 +0545

    add

--- server/index.js
+++ server/index.js
@@ -18,13 +18,14 @@ connectToMongoDB(mongo)
 const signupRoute = require("./routes/signuppage");
 const loginRoute = require("./routes/loginpage");
 const cartRoute = require("./routes/cartRoute");
-// const addItemsRoute = require("./routes/addProductRoute");
+const addItemsRoute = require("./routes/addProductRoute");
 const orderRoute = require("./routes/orderroute");
 const { khalti, khaltiCallback } = require("./khalti");
 app.use("/signup", signupRoute);
 app.use("/login", loginRoute);
 app.use("/cart", cartRoute);
-// app.use("/addProduct", addItemsRoute);
+app.use("/addProduct", addItemsRoute);
+app.use("/uploads", express.static("uploads"));
 app.use("/api/orderSchedule", orderRoute);
 app.use("/api/khalti/init", khalti);
 app.use("/api/khalti/init/verify", khaltiCallback);
